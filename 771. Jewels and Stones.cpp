ca2a1ec1d22700e7e584793a53f9eea36ffe43ee
liufengling <929407814@qq.com>
2019-01-23 10:45:43 +0800

    Update 771. Jewels and Stones.cpp

--- 771. Jewels and Stones.cpp
+++ 771. Jewels and Stones.cpp
@@ -4,13 +4,11 @@ class Solution {
 public:
     int numJewelsInStones(string J, string S) {
         int num=0;
-        for(int i=0;i<S.length();i++)
-        {
-		for(int j=0;j<J.length();j++)
-		{
-		    if(S[i]==J[j])
-			num++;
-		}
+        for(int i=0;i<S.length();i++){
+	    for(int j=0;j<J.length();j++){
+	        if(S[i]==J[j])
+		    num++;
+	    }
         }
         return num;
 };
@@ -24,13 +22,12 @@ public:
 	int index = -1,total = 0;
 
 	for (int ix = 0;ix < J.length(); ++ix){
-		alphabet[J[ix] - 0x41] = 1;
-		std::cout<<J[ix] - 0x41<<endl;
+	    alphabet[J[ix] - 0x41] = 1;
+	    std::cout<<J[ix] - 0x41<<endl;
 	}
-
 	for (int ix = 0;ix < S.length(); ++ix){
-		index = S[ix] - 0x41;
-		total = alphabet[index] ==1 ? ++total:total;
+	    index = S[ix] - 0x41;
+	    total = alphabet[index] ==1 ? ++total:total;
 	}
-	return total;
+        return total;
 };
