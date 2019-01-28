b3062e7931597eafcdb17a926b5b17c18b588eec
liufengling <929407814@qq.com>
2019-01-28 15:14:07 +0800

    Update 423. Reconstruct Original Digits from English.cpp

--- 423. Reconstruct Original Digits from English.cpp
+++ 423. Reconstruct Original Digits from English.cpp
@@ -1,1 +1,35 @@
-hello
+class Solution {
+public:
+    string originalDigits(string s) {
+    string rets;
+
+	if (s.empty()) {
+		return rets;
+	}
+
+	int counts[26] = {0}, len = s.length();
+	int nums[10] = {0};
+
+	for(int ix = 0; ix < len; ++ix) {
+		++counts[s[ix] - 'a'];
+	}
+
+	nums[0] = counts['z' - 'a'];
+	nums[2] = counts['w' - 'a'];
+	nums[4] = counts['u' - 'a'];
+	nums[6] = counts['x' - 'a'];
+	nums[8] = counts['g' - 'a'];
+	nums[1] = counts['o' - 'a'] - nums[0] - nums[2] - nums[4];
+	nums[3] = counts['t' - 'a'] - nums[2] - nums[8];
+	nums[5] = counts['f' - 'a'] - nums[4];
+	nums[7] = counts['s' - 'a'] - nums[6];
+	nums[9] = counts['i' - 'a'] - nums[5] - nums[6] - nums[8];
+
+   
+	for (int ix = 0; ix < 10; ++ix) {
+		rets += string(nums[ix], '0' + ix);
+	}
+
+	return rets;
+    }
+};
